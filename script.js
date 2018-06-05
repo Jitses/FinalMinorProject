8448ffbcd6f8ed6f243ac47d5c7550e0a108fa37
Jitse Schol <jitseschol@hotmail.com>
2018-06-05 15:22:52 +0200

    plan

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+ * Jitse Schol
+ * Programmeerproject
+ * Student Number: 10781463
+ * 5-5-2018
+ */
+document.onload = function(){
+  
+  // http://datamaps.github.io/
+  var map = new Datamap({
+  element: document.getElementById('container'),
+  fills: {
+            defaultFill: 'red'
+        }
+  });
+}
